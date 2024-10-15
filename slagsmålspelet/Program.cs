ed694ea53db618505f1f60e1e87403636632d8a2
Gustav.Nordlander <Gustav.nordlander@elev.ga.ntig.se>
2024-10-15 12:57:51 +0200

    jytnyht

---
+++ slagsmålspelet/Program.cs
@@ -0,0 +1,42 @@
+﻿string Shono1;
+string Shono2;
+int hp1 = 10;
+int hp2 = 10;
+
+Console.WriteLine("skriv spelare etts namn sedan spelare 2s namn");
+
+Shono1 = Console.ReadLine();
+Shono2 = Console.ReadLine();
+
+
+
+
+while (hp1 > 0 || hp2 > 0) 
+{
+    Console.WriteLine("tja boys");
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Console.WriteLine(Shono1 + " & " + Shono2);
+Console.WriteLine(hp1 +" & "+ hp2);
+
+Console.ReadLine();
